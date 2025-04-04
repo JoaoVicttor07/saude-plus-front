57eb45ab484a6a1e224ef6b70f2e00be1652bd6e
Vitoriohfp <128873783+Vitoriohfp@users.noreply.github.com>
2025-04-03 23:44:22 -0300

    Faz tela de cadastro.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Signin from './pages/Signin/index'
-
-
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin/index';
+import Signup from './pages/signup/index';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Signin />} />
-        {}
+        <Route path="/register" element={<Signup />} />
       </Routes>
     </Router>
   );
