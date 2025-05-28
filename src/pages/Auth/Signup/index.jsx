90ee19692cca8677e9ca0e81faaa43248fdfe4d1
João Victor <157769029+JoaoVicttor07@users.noreply.github.com>
2025-05-27 21:22:42 -0300

    fix(cadastro): resolve bug de container quebrando a tela

--- src/pages/Auth/Signup/index.jsx
+++ src/pages/Auth/Signup/index.jsx
@@ -48,62 +48,64 @@ function Signup() {
   };
 
   return (
-    <div className="signup-container">
-      <h1>Cadastro de Paciente</h1>
-      <form
-        className="signup-form"
-        onSubmit={step === 1 ? handleNext : handleSubmit}
-      >
-        {step === 1 && (
-          <>
-            <label htmlFor="nome">Nome completo</label>
-            <Input
-              type="text"
-              id="nome"
-              name="nome"
-              placeholder="Digite seu nome completo"
-              value={form.nome}
-              onChange={handleChange}
-              required
-            />
+    <div className="mainContainer">
 
-            <label htmlFor="cpf">CPF</label>
-            <Input
-              type="text"
-              id="cpf"
-              name="cpf"
-              placeholder="Digite seu CPF"
-              value={form.cpf}
-              onChange={handleChange}
-              required
-            />
+      <div className="signup-container">
+        <h1>Cadastro de Paciente</h1>
+        <form
+          className="signup-form"
+          onSubmit={step === 1 ? handleNext : handleSubmit}
+        >
+          {step === 1 && (
+            <>
+              <label htmlFor="nome">Nome completo</label>
+              <Input
+                type="text"
+                id="nome"
+                name="nome"
+                placeholder="Digite seu nome completo"
+                value={form.nome}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="telefone">Telefone</label>
-            <Input
-              type="text"
-              id="telefone"
-              name="telefone"
-              placeholder="Digite seu telefone"
-              value={form.telefone}
-              onChange={handleChange}
-              required
-            />
+              <label htmlFor="cpf">CPF</label>
+              <Input
+                type="text"
+                id="cpf"
+                name="cpf"
+                placeholder="Digite seu CPF"
+                value={form.cpf}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="sexo">Gênero</label>
-            <select
-              id="sexo"
-              name="sexo"
-              value={form.sexo}
-              onChange={handleChange}
-              required
-            >
-              <option value="">Selecione</option>
-              <option value="masculino">Masculino</option>
-              <option value="feminino">Feminino</option>
-              <option value="outro">Outro</option>
-            </select>
+              <label htmlFor="telefone">Telefone</label>
+              <Input
+                type="text"
+                id="telefone"
+                name="telefone"
+                placeholder="Digite seu telefone"
+                value={form.telefone}
+                onChange={handleChange}
+                required
+              />
 
-            {/* <label htmlFor="alergia">
+              <label htmlFor="sexo">Gênero</label>
+              <select
+                id="sexo"
+                name="sexo"
+                value={form.sexo}
+                onChange={handleChange}
+                required
+              >
+                <option value="">Selecione</option>
+                <option value="masculino">Masculino</option>
+                <option value="feminino">Feminino</option>
+                <option value="outro">Outro</option>
+              </select>
+
+              {/* <label htmlFor="alergia">
               Alergia/Doença específica (opcional)
             </label>
             <Input
@@ -115,74 +117,75 @@ function Signup() {
               onChange={handleChange}
             /> */}
 
-            <Button
-              background="#2c7a7b"
-              color="#fff"
-              border="none"
-              borderRadius="4px"
-              height="40px"
-              hoverBackground="#285e61"
-              style={{
-                outline: "none",
-                boxShadow: "none",
-              }}
-            >
-              Próxima etapa
-            </Button>
-          </>
-        )}
+              <Button
+                background="#2c7a7b"
+                color="#fff"
+                border="none"
+                borderRadius="4px"
+                height="40px"
+                hoverBackground="#285e61"
+                style={{
+                  outline: "none",
+                  boxShadow: "none",
+                }}
+              >
+                Próxima etapa
+              </Button>
+            </>
+          )}
 
-        {step === 2 && (
-          <>
-            <label htmlFor="email">Email</label>
-            <Input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Digite seu email"
-              value={form.email}
-              onChange={handleChange}
-              required
-            />
+          {step === 2 && (
+            <>
+              <label htmlFor="email">Email</label>
+              <Input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Digite seu email"
+                value={form.email}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="password">Senha</label>
-            <Input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Digite sua senha"
-              value={form.password}
-              onChange={handleChange}
-              required
-            />
+              <label htmlFor="password">Senha</label>
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Digite sua senha"
+                value={form.password}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="confirmPassword">Confirmar Senha</label>
-            <Input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              placeholder="Confirme sua senha"
-              value={form.confirmPassword}
-              onChange={handleChange}
-              required
-            />
-            <Button>Cadastrar</Button>
+              <label htmlFor="confirmPassword">Confirmar Senha</label>
+              <Input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                placeholder="Confirme sua senha"
+                value={form.confirmPassword}
+                onChange={handleChange}
+                required
+              />
+              <Button>Cadastrar</Button>
 
-            <Button
-              onClick={handleBack}
-              background="grey"
-              style={{
-                marginTop: "8px",
-              }}
-            >
-              Voltar
-            </Button>
-          </>
-        )}
-      </form>
-      <p className="register-link">
-        Já tem uma conta? <Link href="/signin">Entrar</Link>
-      </p>
+              <Button
+                onClick={handleBack}
+                background="grey"
+                style={{
+                  marginTop: "8px",
+                }}
+              >
+                Voltar
+              </Button>
+            </>
+          )}
+        </form>
+        <p className="register-link">
+          Já tem uma conta? <Link href="/signin">Entrar</Link>
+        </p>
+      </div>
     </div>
   );
 }
